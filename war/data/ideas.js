d944c7ceaa7f95e3fdd3a9f6bf5da4f7e7822507
Chica Goillinois <skinintheg4me@gmail.com>
2011-09-27 17:58:53 -0500

    use mindy's suggestions for polls and ideas

--- war/data/ideas.js
+++ war/data/ideas.js
@@ -4,7 +4,7 @@ define(function(){
 	    "id": "1",
 	    "title": "Clean-Up Group",
 	    "first": true,
-	    "description": "I want to help clean up the lot before it becomes a park. Could we get a clean-up group organized for this? I’m available on the weekends and after school on weekdays."
+	    "description": "I want to help clean up the lot before it becomes a park. Could we get a clean-up group organized for this? I'm available on the weekends and after school on weekdays."
 	  }, {
 	    "id": "2",
 	    "title": "Latin Music Fest",
@@ -16,11 +16,11 @@ define(function(){
 	  }, {
 	    "id": "4",
 	    "title": "Time for Girls",
-	    "description": "My friends and I want to play basketball but there is always a group of guys on the court. Could we have a “Girl-time only” for the field & basketball courts?"
+	    "description": "My friends and I want to play basketball but there is always a group of guys on the court. Could we have a 'Girl-time only' for the field & basketball courts?"
 	  }, {
 	    "id": "5",
-	    "title": "I can't reach",
-	    "description": "I’m only 12 but I want to play basketball in my park, could we have a kids size basketball park?"
+	    "title": "I Can't Reach",
+	    "description": "I'm only 12 but I want to play basketball in my park, could we have a kids size basketball park?"
 	  }, {
 	    "id": "6",
 	    "title": "Graffiti Art",
@@ -28,7 +28,7 @@ define(function(){
 	  }, {
 	    "id": "7",
 	    "title": "Neighborhood Watch",
-	    "description": "How are we going to keep our park safe? This concerned mother needs to know her kids are hanging out in a safe place. If we could organize a community watch I’d be glad to participate. I’m a stay-at-home mom so I could keep my eye on the north-east side of the park Monday through Friday 9-3. Anyone else willing to lend a hand?"
+	    "description": "How are we going to keep our park safe? This concerned mother needs to know her kids are hanging out in a safe place. If we could organize a community watch I'd be glad to participate. I'm a stay-at-home mom so I could keep my eye on the north-east side of the park Monday through Friday 9-3. Anyone else willing to lend a hand?"
 	  }
 	]
 });
--- war/data/polls.js
+++ war/data/polls.js
@@ -6,26 +6,38 @@ define(function(){
 	    "description": "What should we name the park? Based on name submissions we are now voting on the top 3!",
 	    "multiple": false,
 	    "first": true,
-	    "choices": [{"index": "0", "choice": "Parque de Ellen Ochoa"}, {"index": "1", "choice": "Parque de Ana Castillo"}, {"index": "2", "choice": "Parque de Frida Kahlo"}]
-	  }, 
+	    "choices": [
+	       {"index": "0", "choice": "Parque Marentes", "detail": "labor organizer, farm worker and environmental justice advocate"}, 
+	       {"index": "1", "choice": "Parque Cisneros", "detail": "Chicago born Chicana writer and author of the House on Mango Street"}, 
+	       {"index": "2", "choice": "Parque Castillo", "detail": "Mexican-American Chicana novelist, poet, short story writer, and essayist"},
+	       {"index": "3", "choice": "Parque Sarate", "detail": "Director of Little Village Community Development Corporation"}
+	     ]
+	  },
 	  {
 	    "id": "2",
-	    "title": "El Grito Parade Theme",
-	    "description": "What should be the theme for this year's El Grito parade?",
+	    "title": "Cage Free Chicken Coops?",
+	    "description": "City Farm has set up cage free chicken coops at their urban farm. Do you think people in Little Village should be allowed to have cage free chicken coops in their backyards?",
 	    "multiple": true,
 	    "choices": [{"index": "0", "choice":"Mi familia"},{"index": "1", "choice":"Viva la Raza"},{"index": "2", "choice":"la communidad"}]
 	  }, 
 	  {
 	    "id": "3",
-	    "title": "Reopen Museum?",
-	    "description": "Should the Museum of Mexican Culture and History be reopened?",
+	    "title": "Help Reopen Cafe Catedral?",
+	    "description": "Cafe Catedral recently had to close because of the bad economy. Do you think the community and alderman should work together to get it re-opened?",
 	    "multiple": false,
 	    "choices": [{"index": "0", "choice":"Yes"},{"index": "1", "choice":"No"}]
-	  },
+	  }, 
 	  {
 	    "id": "4",
-	    "title": "Arts and Crafts Festival?",
-	    "description": "Should there be an annual Arts and Crafts Festival in the park?",
+	    "title": "Asthma Van",
+		"description": "If you child has asthma or breathing problems, how often does your child needed to be treated by the asthma van at school?",
+		"multiple": false,
+		"choices": [{"index": "0", "choice": "Once a week or more"}, {"index": "1", "choice": "Once a month or more"}, {"index": "2", "choice": "Once every few months"}, {"index": "3", "choice": "Once or twice a year"}]
+      },
+	  {
+	    "id": "5",
+	    "title": "Plots for Elementary Schools?",
+	    "description": "Do you think that our neighborhood elementary schools should have plots in the new community garden in Mi Parque?",
 	    "multiple": false,
 	    "choices": [{"index": "0", "choice":"Yes"},{"index": "1", "choice":"No"}]
 	  }
