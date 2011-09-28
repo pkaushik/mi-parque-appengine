be055f7066b29573131e939e333bf1c7a075ec00
Chica Goillinois <skinintheg4me@gmail.com>
2011-09-28 18:50:34 -0500

    move gis files

--- war/src/views/Placemaking.js
+++ war/src/views/Placemaking.js
@@ -1,10 +1,10 @@
 define([
-  '../../data/boulevards_little_village',
-  '../../data/campus_parks_little_village',
-  '../../data/habitat_little_village',
-  '../../data/mall_plaza_little_village',
-  '../../data/neighborspace_gardens_little_village',
-  '../../data/riverwalk_little_village'
+  '../../data/gis/boulevards_little_village',
+  '../../data/gis/campus_parks_little_village',
+  '../../data/gis/habitat_little_village',
+  '../../data/gis/mall_plaza_little_village',
+  '../../data/gis/neighborspace_gardens_little_village',
+  '../../data/gis/riverwalk_little_village'
 ], function(boulevards, campus_parks, habitat, mall_plaza, neighborspace, riverwalk) {
   return Backbone.View.extend({
 	  
