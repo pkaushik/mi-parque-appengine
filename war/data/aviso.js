394ef194da0287af55dd1956a9f2589bdb772e07
Chica Goillinois <skinintheg4me@gmail.com>
2011-10-01 00:15:46 -0500

    update aviso

--- war/data/aviso.js
+++ war/data/aviso.js
@@ -1,36 +1,94 @@
 define(function(){ 
 	return [
-	  {
-	    "id": "1",
-	    "title": "Field Trip to City Farm",
-	    "type": "community",
-	    "lat": 20,
-	    "lon": 20,
-	    "description": "LVEJO is organizing a field trip to visit City Farm at Clybourn and Division next spring to see how the Resource Center plants 35 varieties of tomatoes, beets, carrots and lettuces. Learn how to obtain gourmet seeds at reasonable prices. Call LVEJO to sign up for the tour."
-	  }, 
-	  {
-	    "id": "2",
-	    "title": "Air Quality Alert",
-	    "type": "air",
-	    "lat": 20,
-	    "lon": 20,
-	    "description": "..."
-	  },
-	  {
-	    "id": "3",
-	    "title": "Fire Evacuation Plan",
-	    "type": "community",
-	    "lat": 20,
-	    "lon": 20,
-	    "description": "In September, a fire broke out at the Crawford Coal Power Plant. As a result, residents realized that there is no evacuation plan for Little Village. A public meeting will take place the Little Village Social Justice High School Auditorium at 7:00 on  October 21st to discuss with our alderman and city officials."
-	  }, 
-	  {
-	    "id": "4",
-	    "title": "Asthma Van",
-	    "type": "asthma",
-	    "lat": 20,
-	    "lon": 20,
-	    "description": "..."
-	  } 
-	]
+	        {
+	            "id": 14,
+	            "title": "Close Chicago\u0027s Toxic Coal Plants!",
+	            "type": "community",
+	            "lat": 41.84392980,
+	            "lon": -87.7172194,
+	            "description": "At 11:00am today, six activists with the Little Village Environmental Justice Organization (LVEJO), Rising Tide North America, Rainforest Action Network (RAN) and the Backbone Campaign were arrested after climbing the fence to Midwest Generation?s controversial Crawford coal plant in Little Village. The activists unfurled a 7? x 30? banner atop a 20 foot tall sprawling coal pile that feeds the power plant, which reads: ?Close Chicago?s Toxic Coal Plants.?",
+	            "imgURL": "http://www.chicagocleanpower.org/wp-content/uploads/2011/04/IMG_02_580x350.jpg"
+	        },
+	        {
+	            "id": 13,
+	            "title": "EPA to Hold Public Meeting",
+	            "type": "community",
+	            "lat": 41.8780690,
+	            "lon": -87.630440,
+	            "description": "The EPA Region 5 is holding a meeting on October 20, 2011 concerning the air, water and soil conditions surrounding coal power plants. The meeting is public and community members of affected areas are encouraged to attend.",
+	            "imgURL": "http://farm3.static.flickr.com/2212/2472399970_5f75b9bd2e_z.jpg"
+	        },
+	        {
+	            "id": 12,
+	            "title": "Park Clean-Up",
+	            "type": "community",
+	            "lat": 41.83732890,
+	            "lon": -87.6999032,
+	            "description": "As part of Mi Idea, I will be organizing a clean-up group to begin preparing the Superfund site for the construction of our park. Our first order of business will be to pick up all the trash that has accumulated in and around the lot. Anyone who is interested in helping clean up the site, please send me an email at chica_goillinois@gmail.com. The date of clean up is: TBA.",
+	            "imgURL": "http://farm7.static.flickr.com/6163/6173405978_72e3232ff2_z.jpg"
+	        },
+	        {
+	            "id": 11,
+	            "title": "Name the Park",
+	            "type": "community",
+	            "lat": 41.83732890,
+	            "lon": -87.6999032,
+	            "description": "Voting is now taking place for the naming of our new park. Click on the \u0027Mi Voto\u0027 tab to view and vote on your favorite.",
+	            "imgURL": "http://farm7.static.flickr.com/6159/6199052510_b69fecac1f_z.jpg"
+	        },
+	        {
+	            "id": 10,
+	            "title": "Field Trip to City Farm",
+	            "type": "community",
+	            "lat": 41.9039670,
+	            "lon": -87.640584,
+	            "description": "LVEJO is organizing a field trip to visit City Farm at Clybourn and Division next spring to see how the Resource Center plants 35 varieties of tomatoes, beets, carrots and lettuces. Learn how to obtain gourmet seeds at reasonable prices. Call LVEJO to sign up for the tour. ",
+	            "imgURL": "http://www.farmaid.org/atf/cf/%7B6ef41923-f003-4e0f-a4a6-ae0031db12fb%7D/homepage-urban_farming.jpg"
+	        },
+	        {
+	            "id": 9,
+	            "title": "Asthma Outbreak in La Villita",
+	            "type": "air",
+	            "lat": 41.84392980,
+	            "lon": -87.7172194,
+	            "description": "On Sept 14, an air pollution action day was declared for Cook County triggered by Minnesota wildfires. Asthma vans were dispatched to three Little Village schools  to treat several children experiencing breathing difficulty. Elevated levels of fine particulate matter pose health risks to sensitive populations, especially individuals with respiratory or pulmonary disorders as well as active children and adults. This is the 7th action alert in Little Village in 2011.",
+	            "imgURL": "http://lvejo.org/wp-content/ftpuploads/2011/09/0Sierra-Club-Photos2.jpg"
+	        },
+	        {
+	            "id": 8,
+	            "title": "Five children treated at Cardenas",
+	            "type": "asthma",
+	            "lat": 41.84872750,
+	            "lon": -87.71608540,
+	            "description": "Asthma vans dispatched to Cardenas treated five children during week of September 12-16th",
+	            "imgURL": "http://www.chrisdraftfamilyfoundation.org/tools/founder_features/files/DSC_0269_rs2.jpg"
+	        },
+	        {
+	            "id": 7,
+	            "title": "Air Quality Alert",
+	            "type": "air",
+	            "lat": 41.83921,
+	            "lon": -87.65410,
+	            "description": "Air Quality Alert",
+	            "imgURL": "http://mi-parque.appspot.com/images/airpollution.jpg"
+	        },
+	        {
+	            "id": 6,
+	            "title": "Fire Evacuation Plan",
+	            "type": "community",
+	            "lat": 41.68266,
+	            "lon": -87.53981,
+	            "description": "In September, a fire broke out at the Crawford Coal Power Plant. As a result, residents realized that there is no evacuation plan for Little Village. A public meeting will take place the Little Village Social Justice High School Auditorium at 7:00 on  October 21st to discuss with our alderman and city officials.",
+	            "imgURL": "http://mi-parque.appspot.com/images/firecoal.jpg"
+	        },
+	        {
+	            "id": 5,
+	            "title": "Save Little Village\u0027s Post Office",
+	            "type": "community",
+	            "lat": 41.85131,
+	            "lon": -87.63330,
+	            "description": "11 VItal Post Offices Set to Close  LVEJO invites you to Join us at a RALLY to Save the Post Office\u0027s  and 10 other Post Offices Vital to Communities in Chicago.",
+	            "imgURL": "http://lvejo.org/wp-content/ftpuploads/2010/07/00slide_cta580x350_05.jpg"
+	        }
+	    ]
 });
