f59a2ea129d031632b884dfd64c7e52965290935
Chica Goillinois <skinintheg4me@gmail.com>
2011-10-30 21:29:30 -0500

    move postPoll function from main to PollCollection where it belongs

--- war/src/main.js
+++ war/src/main.js
@@ -16,25 +16,6 @@ function scrollToBottom() {
 	$.mobile.silentScroll($(document).height());
 }
 
-function postPoll(id, title, choice) {
-	var verb = '/me/miparque:voice';
-	var poll_url = 'http://mi-parque.appspot.com/pages/polls/';
-	var token = 'AAADZC8pSyZAXgBANTS4LWmpU6ssNYjK7whHGwRTdFlGpKgLWhkThADqZAXEviNfUvfaoGY03G7TxQcZCEE8Fy5vXiWFNMGknvIq0tXWrQTn3O3Ek87Y6';
-	var path = convertToSlug([ id, title ].join(" "));
-	var slug = convertToSlug([ choice ].join(" "));
-	var og_action = verb + '&access_token=' + token + '&poll=' + poll_url
-			+ path + '/' + slug + '.html';
-
-	console.log(og_action);
-	FB.api(og_action, 'post', function(response) {
-		if (!response || response.error) {
-			console.log(response);
-		} else {
-			console.log('It worked. id: ' + response.id);
-		}
-	});
-}
-
 
 $('#idea').live('pagecreate', function(event) {
 	$('#idea').trigger('create');
