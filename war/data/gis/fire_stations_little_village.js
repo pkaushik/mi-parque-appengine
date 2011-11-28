d5c36baf25c1dd9baf775416626b77c5fa84735f
Chica Goillinois <skinintheg4me@gmail.com>
2011-11-27 22:52:59 -0600

    add GIS data for schools, fire stations, police stations

---
+++ war/data/gis/fire_stations_little_village.js
@@ -0,0 +1,339 @@
+{
+  "meta" : {
+    "view" : {
+      "id" : "azew-3y2s",
+      "name" : "Fire Stations - Map",
+      "attribution" : "City of Chicago",
+      "attributionLink" : "http://www.cityofchicago.org",
+      "averageRating" : 0,
+      "category" : "Public Safety",
+      "description" : "Fire stations located in Chicago",
+      "displayType" : "table",
+      "moderationStatus" : true,
+      "numberOfComments" : 0,
+      "oid" : 0,
+      "publicationAppendEnabled" : false,
+      "publicationStage" : "unpublished",
+      "rowClass" : "",
+      "rowsUpdatedAt" : 1313894852,
+      "rowsUpdatedBy" : "scy9-9wg4",
+      "signed" : false,
+      "tableId" : 220747,
+      "totalTimesRated" : 0,
+      "viewType" : "tabular",
+      "columns" : [ {
+        "id" : -1,
+        "name" : "sid",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "sid",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : -1,
+        "name" : "id",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "id",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : -1,
+        "name" : "position",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "position",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : -1,
+        "name" : "created_at",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "created_at",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : -1,
+        "name" : "created_meta",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "created_meta",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : -1,
+        "name" : "updated_at",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "updated_at",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : -1,
+        "name" : "updated_meta",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "updated_meta",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : -1,
+        "name" : "meta",
+        "dataTypeName" : "meta_data",
+        "fieldName" : "meta",
+        "position" : 0,
+        "renderTypeName" : "meta_data",
+        "format" : {
+        }
+      }, {
+        "id" : 2615130,
+        "name" : "NAME",
+        "dataTypeName" : "text",
+        "fieldName" : "name",
+        "position" : 1,
+        "renderTypeName" : "text",
+        "tableColumnId" : 1424810,
+        "width" : 69,
+        "format" : {
+        }
+      }, {
+        "id" : 2615131,
+        "name" : "ADDRESS",
+        "dataTypeName" : "text",
+        "fieldName" : "address",
+        "position" : 2,
+        "renderTypeName" : "text",
+        "tableColumnId" : 1424811,
+        "width" : 154,
+        "format" : {
+        }
+      }, {
+        "id" : 2615132,
+        "name" : "CITY",
+        "dataTypeName" : "text",
+        "fieldName" : "city",
+        "position" : 3,
+        "renderTypeName" : "text",
+        "tableColumnId" : 1424812,
+        "width" : 118,
+        "format" : {
+        }
+      }, {
+        "id" : 2615133,
+        "name" : "STATE",
+        "dataTypeName" : "text",
+        "fieldName" : "state",
+        "position" : 4,
+        "renderTypeName" : "text",
+        "tableColumnId" : 1424813,
+        "width" : 50,
+        "format" : {
+        }
+      }, {
+        "id" : 2615134,
+        "name" : "ZIP",
+        "dataTypeName" : "text",
+        "fieldName" : "zip",
+        "position" : 5,
+        "renderTypeName" : "text",
+        "tableColumnId" : 1424816,
+        "width" : 50,
+        "format" : {
+          "precisionStyle" : "standard",
+          "align" : "right"
+        }
+      }, {
+        "id" : 2615135,
+        "name" : "ENGINE",
+        "dataTypeName" : "text",
+        "fieldName" : "engine",
+        "position" : 6,
+        "renderTypeName" : "text",
+        "tableColumnId" : 1424815,
+        "width" : 81,
+        "format" : {
+        }
+      }, {
+        "id" : 2615136,
+        "name" : "LOCATION",
+        "dataTypeName" : "location",
+        "fieldName" : "location",
+        "position" : 7,
+        "renderTypeName" : "location",
+        "tableColumnId" : 1424817,
+        "width" : 100,
+        "format" : {
+        },
+        "subColumnTypes" : [ "human_address", "latitude", "longitude", "machine_address", "needs_recoding" ]
+      } ],
+      "displayFormat" : {
+        "viewport" : {
+          "center" : {
+            "lng" : -87.75304707762155,
+            "lat" : 41.84467578395129
+          },
+          "ymin" : 41.821014,
+          "ymax" : 41.868329,
+          "zoom" : 13,
+          "xmin" : -87.838792,
+          "xmax" : -87.667302
+        },
+        "plotStyle" : "point",
+        "type" : "google",
+        "clusterMarkers" : false,
+        "plot" : {
+          "descriptionColumns" : [ {
+            "tableColumnId" : 1424817
+          } ],
+          "locationId" : 1424817,
+          "titleId" : 1424810
+        }
+      },
+      "grants" : [ {
+        "inherited" : true,
+        "type" : "viewer",
+        "flags" : [ "public" ]
+      } ],
+      "metadata" : {
+        "custom_fields" : {
+          "Metadata" : {
+            "Data Owner" : "Fire",
+            "Time Period" : "Current as of 2011",
+            "Frequency" : "Data is updated as needed"
+          }
+        },
+        "renderTypeConfig" : {
+          "visible" : {
+            "table" : true
+          }
+        },
+        "availableDisplayTypes" : [ "map", "table", "fatrow", "page" ],
+        "rdfSubject" : "0",
+        "attachments" : [ {
+          "blobId" : "05882A26-5DE0-458D-A046-0FA87AC76F6A",
+          "name" : "fire_stations.zip",
+          "filename" : "fire_stations.zip"
+        } ],
+        "rowIdentifier" : "0",
+        "rdfClass" : ""
+      },
+      "query" : {
+        "filterCondition" : {
+          "type" : "operator",
+          "value" : "AND",
+          "children" : [ {
+            "type" : "operator",
+            "value" : "OR",
+            "children" : [ {
+              "type" : "operator",
+              "value" : "EQUALS",
+              "children" : [ {
+                "columnId" : 2615130,
+                "type" : "column"
+              }, {
+                "type" : "literal",
+                "value" : "E99"
+              } ],
+              "metadata" : {
+                "freeform" : true
+              }
+            }, {
+              "type" : "operator",
+              "value" : "EQUALS",
+              "children" : [ {
+                "columnId" : 2615130,
+                "type" : "column"
+              }, {
+                "type" : "literal",
+                "value" : "E109"
+              } ],
+              "metadata" : {
+                "freeform" : true
+              }
+            } ],
+            "metadata" : {
+              "tableColumnId" : {
+                "220747" : 1424810
+              },
+              "operator" : "EQUALS"
+            }
+          } ],
+          "metadata" : {
+            "unifiedVersion" : 2
+          }
+        }
+      },
+      "rights" : [ "read" ],
+      "tableAuthor" : {
+        "id" : "scy9-9wg4",
+        "displayName" : "cocadmin",
+        "emailUnsubscribed" : false,
+        "privacyControl" : "login",
+        "profileImageUrlLarge" : "/images/profile/4783/7574/CitySeal_Small_large.jpg",
+        "profileImageUrlMedium" : "/images/profile/4783/7574/CitySeal_Small_thumb.jpg",
+        "profileImageUrlSmall" : "/images/profile/4783/7574/CitySeal_Small_tiny.jpg",
+        "profileLastModified" : 1320762424,
+        "roleName" : "administrator",
+        "screenName" : "cocadmin",
+        "rights" : [ "create_datasets", "edit_others_datasets", "edit_sdp", "edit_site_theme", "moderate_comments", "manage_users", "chown_datasets", "edit_nominations", "approve_nominations", "feature_items", "federations", "manage_stories", "manage_approval", "change_configurations", "view_domain", "view_others_datasets" ]
+      },
+      "tags" : [ "facilities", "gis" ],
+      "viewFilters" : {
+        "type" : "operator",
+        "value" : "AND",
+        "children" : [ {
+          "type" : "operator",
+          "value" : "OR",
+          "children" : [ {
+            "type" : "operator",
+            "value" : "EQUALS",
+            "children" : [ {
+              "columnId" : 2615130,
+              "type" : "column"
+            }, {
+              "type" : "literal",
+              "value" : "E99"
+            } ],
+            "metadata" : {
+              "freeform" : true
+            }
+          }, {
+            "type" : "operator",
+            "value" : "EQUALS",
+            "children" : [ {
+              "columnId" : 2615130,
+              "type" : "column"
+            }, {
+              "type" : "literal",
+              "value" : "E109"
+            } ],
+            "metadata" : {
+              "freeform" : true
+            }
+          } ],
+          "metadata" : {
+            "tableColumnId" : {
+              "220747" : 1424810
+            },
+            "operator" : "EQUALS"
+          }
+        } ],
+        "metadata" : {
+          "unifiedVersion" : 2
+        }
+      },
+      "flags" : [ "unsaved" ]
+    }
+  },
+  "data" : [ [ 13, "5EBA9606-758F-4EC5-BF1D-0630265358B5", 13, 1293051547, "386464", 1293051629, "386464", null, "E99", "3042 S KEDVALE AVE", "CHICAGO", "IL", "60623", "E99", [ "{\"address\":\"3042 S KEDVALE AVE\",\"city\":\"CHICAGO\",\"state\":\"IL\",\"zip\":\"60623\"}", "41.837386198391975", "-87.72806083645415", null, false ] ]
+, [ 52, "2E6F499C-CDAE-4540-B12A-C0757433462C", 52, 1293051547, "386464", 1293051628, "386464", null, "E109", "2358 S WHIPPLE ST", "CHICAGO", "IL", "60623", "E109", [ "{\"address\":\"2358 S WHIPPLE ST\",\"city\":\"CHICAGO\",\"state\":\"IL\",\"zip\":\"60623\"}", "41.84833993168608", "-87.70181130248798", null, false ] ]
+ ]
+}
