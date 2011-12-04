75965b71136f8c9082348c64f72e28066c5bfff8
Chica Goillinois <skinintheg4me@gmail.com>
2011-12-03 22:27:34 -0600

    Add brownfield sites.

--- war/src/views/EPAMapOverlay.js
+++ war/src/views/EPAMapOverlay.js
@@ -1,25 +1,16 @@
 define([
-  '../../data/gis/site_remediation_little_village'
-], function(site) {
+  '../../data/gis/site_remediation_little_village',
+  '../../data/gis/brownfields'
+], function(site, brownfields) {
   return Backbone.View.extend({
-	
 	initialize: function() {
 	  if (typeof google === 'undefined') return this;
-	  
-//	  this.epa = new GeoJSON(epa, {
-//		strokeColor: "#343434",
-//		strokeWeight: 1,
-//		strokeOpacity: 0.75,
-//		fillOpacity: 1,
-//		fillColor: "#343434" 
-//	  });
 	  
 	  this.render();
 	  return this;
 	},
 	
 	renderFeatureCollection: function(collection, map) {
-		console.log(collection)
 	  if (collection.length) {
 	    _(collection).each(function(feature) {
     	  if (feature.length) {
@@ -38,11 +29,19 @@ define([
 	
     render: function() { 
       var that = this;
-      //this.renderFeatureCollection(this.epa, this.options.map);
       _(site.data).each(function(arr){ 
   		  var marker = new google.maps.Marker({
   			position: new google.maps.LatLng(arr[10], arr[11]),
   			icon: 'images/aviso_community_small.png',
+  	        flat: true,
+  	        clickable: false,
+  	        map: that.options.map
+  		  });
+  	  });
+      _(brownfields.data).each(function(arr){ 
+  		  var marker = new google.maps.Marker({
+  			position: new google.maps.LatLng(arr[0], arr[1]),
+  			icon: 'images/brownfield.png',
   	        flat: true,
   	        clickable: false,
   	        map: that.options.map
